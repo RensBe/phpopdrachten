0a92ca36f0db7b64ba6546769604ed22b1413705
RensBe <59955661+RensBe@users.noreply.github.com>
2020-02-21 10:04:10 +0100

    opdracht_3-3

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!--
+* User: Rens Bedijn
+* Date: 5-2-2020
+* Time: 13:43 PM
+* File: index.php
+-->
+
+<footer>
+    Rens Bedijn, IO1A4
+</footer>
+</body>
+</html>
--- template.php
+++ template.php
@@ -1,20 +1,11 @@
-<!--
-* User: Rens Bedijn
-* Date: 5-2-2020
-* Time: 13:43 PM
-* File: index.php
--->
 <?php
 //includes van de pagina
 include "../../includes/header.php";
 include "../../includes/menu.php";
-include "../variabelen.php";
 ?>
 <main id="wrapper">
     <h2>Uitwerkingen</h2>
-    <?php
-    echo $year;
-    ?>
+
 </main>
 <?php
 include "../../includes/footer.php";
