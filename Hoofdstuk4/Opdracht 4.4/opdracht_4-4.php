e3e0d6114c267c59a4c4c6ebf47bd3689745b2d9
RensBe <59955661+RensBe@users.noreply.github.com>
2020-03-15 15:44:38 +0100

    Oplevering opdracht 4.4

--- Hoofdstuk4/Opdracht 4.4/opdracht_4-4.php
+++ Hoofdstuk4/Opdracht 4.4/opdracht_4-4.php
@@ -5,7 +5,35 @@ include "../../includes/menu.php";
 ?>
     <main id="wrapper">
         <h2>Uitwerkingen</h2>
-
+        <?php
+        for($i = 0; $i <= 7; $i++) {
+            $adder = strtotime("now +" . $i . "days");
+            $day = date('w', $adder);
+            $fullday = date('l d-n-Y', $adder);
+            echo "Dag " . $day . " is " . $fullday.'<br>';
+        }
+        $over = "";
+        echo "<br><br>";
+        For($i = 0; $i <= 7; $i++)
+        {
+            if($i == 0)
+            {
+                $day = "Vandaag";
+            }
+            else
+            {
+                $day = "Morgen";
+            }
+            $adder = strtotime("now +" . $i . "days");
+            $fullday = date('l d-n-Y', $adder);
+            While ($i >= 2)
+            {
+                $over = $over."Over-";
+                break;
+            }
+            echo $over.$day . " is " . $fullday.'<br>';
+        }
+        ?>
     </main>
 <?php
 include "../../includes/footer.php";
