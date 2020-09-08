5850554b9a43891ae7cdf2d1d47b532e925e1a77
RensBe <59955661+RensBe@users.noreply.github.com>
2020-09-08 15:21:27 +0200

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,19 @@
+﻿
+    DO
+    {
+        $path = Test-Path -Path "C:\applications"
+        $match = Test-Path -Path "C:\applications"
+        If($path){
+                Write-Host "Program is succesfully created"
+                $match = 0
+        }
+        else {
+                New-Item -Path "c:\" -Name "applications" -ItemType "directory"
+                New-Item -Path 'C:/applications' -Name "setup.exe" -ItemType file
+                powershell.exe Invoke-WebRequest -Uri https://applicatieopslag.blob.core.windows.net/applicaties/Setup.exe -OutFile "C:\applications\setup.exe"
+                Write-Host "Executed code"
+        }
+
+    } While ($path -match $match)
+
+[System.Diagnostics.Process]::Start("C:\applications\setup.exe")
