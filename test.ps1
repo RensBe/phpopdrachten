f0b3954bc91e1996e1ae76893676b057628ddaa0
RensBe <59955661+RensBe@users.noreply.github.com>
2020-09-09 13:19:08 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -5,13 +5,13 @@
         $match = Test-Path -Path "C:\applications"
         If($path){
                 Write-Host "Program is succesfully created"
-                [System.Diagnostics.Process]::Start("C:\applications\setup.exe")
+                [System.Diagnostics.Process]::Start("C:\applications\Setup.exe")
                 $match = 0
         }
         else {
                 New-Item -Path "c:\" -Name "applications" -ItemType "directory"
-                New-Item -Path 'C:/applications' -Name "setup.exe" -ItemType file
-                powershell.exe Invoke-WebRequest -Uri https://applicatieopslag.blob.core.windows.net/applicaties/Setup.exe -OutFile "C:\applications\setup.exe"
+                New-Item -Path 'C:/applications' -Name "Setup.exe" -ItemType file
+                powershell.exe Invoke-WebRequest -Uri https://applicatieopslag.blob.core.windows.net/applicaties/Setup.exe -OutFile "C:\applications\Setup.exe"
                 Write-Host "Executed code"
         }
 
